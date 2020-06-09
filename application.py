a14bcc2e90806e39d61769958cd34d26a37587e5
tonyz3t <dhanjal.n3@gmail.com>
2020-06-09 16:33:20 -0700

    updated requirements

--- application.py
+++ application.py
@@ -1,7 +1,8 @@
 import os
 
-from flask import Flask
+from flask import Flask, session, render_template, request, redirect, url_for
 from flask_socketio import SocketIO, emit
+from flask_session import Session
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
